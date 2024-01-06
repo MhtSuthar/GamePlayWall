865eeb4a081e378ab68244b4ce84230cd4a4b470
mohit <mht.str@gmail.com>
2024-01-06 19:27:32 +0530

    zoom added

--- app/src/main/java/com/gameplay/ui/detail/WallDetails.kt
+++ app/src/main/java/com/gameplay/ui/detail/WallDetails.kt
@@ -10,6 +10,8 @@ import androidx.activity.compose.ManagedActivityResultLauncher
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.gestures.rememberTransformableState
+import androidx.compose.foundation.gestures.transformable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
@@ -31,10 +33,15 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.core.content.ContextCompat
@@ -106,6 +113,15 @@ fun WallDetails(
         }
     }
 
+    var scale by remember { mutableStateOf(1f) }
+    var rotation by remember { mutableStateOf(0f) }
+    var offset by remember { mutableStateOf(Offset.Zero) }
+    val state = rememberTransformableState { zoomChange, offsetChange, rotationChange ->
+        scale *= zoomChange
+        rotation += rotationChange
+        offset += offsetChange
+    }
+
 
     Surface(
         modifier = Modifier.fillMaxSize(),
@@ -157,7 +173,21 @@ fun WallDetails(
                 }
             },
         ) {
-            Box() {
+            Box( Modifier
+                // apply other transformations like rotation and zoom
+                // on the pizza slice emoji
+                .graphicsLayer(
+                    scaleX = scale,
+                    scaleY = scale,
+                    //rotationZ = rotation,
+                    translationX = offset.x,
+                    translationY = offset.y
+                )
+                // add transformable to listen to multitouch transformation events
+                // after offset
+                .transformable(state = state)
+                //.background(Color.Blue)
+                .fillMaxSize()) {
                 /*val model = ImageRequest.Builder(LocalContext.current)
                     .data(imagePath?.image_url)
                     .size(Size.ORIGINAL)
